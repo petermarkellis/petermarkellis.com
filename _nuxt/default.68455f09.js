a3ee8dbe3fb5d41a0168188121bf8f435a9152ae
Super useful studio <superusefulstudio@gmail.com>
2023-11-18 20:57:43 +0100

    Form update
    Updated form colors. Still need to add text area and tie this into netlifies form service

--- _nuxt/default.68455f09.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,y as _}from"./entry.34e24471.js";const n={};function r(e,s){return _(e.$slots,"default")}const o=t(n,[["render",r]]);export{o as default};
---
+++ _nuxt/default.e40bb2c5.js
@@ -0,0 +1,1 @@
+import{_ as t,y as _}from"./entry.9c65cd47.js";const n={};function r(e,s){return _(e.$slots,"default")}const o=t(n,[["render",r]]);export{o as default};
