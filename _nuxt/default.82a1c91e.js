53dfde1b7e78edfeb86a711e00b4326e25150ccc
Super useful studio <superusefulstudio@gmail.com>
2023-11-18 22:09:25 +0100

    Image path fix

--- _nuxt/default.82a1c91e.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,y as _}from"./entry.2229c94e.js";const n={};function r(e,s){return _(e.$slots,"default")}const o=t(n,[["render",r]]);export{o as default};
---
+++ _nuxt/default.b0fce368.js
@@ -0,0 +1,1 @@
+import{_ as t,y as _}from"./entry.ef372439.js";const n={};function r(e,s){return _(e.$slots,"default")}const o=t(n,[["render",r]]);export{o as default};
